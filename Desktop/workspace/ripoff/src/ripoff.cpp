2ae5bd3441a92b0dea63aa43b81cffabafd1ed30
Sau00 <sau00gm@gmail.com>
2014-11-09 03:50:55 +0300

    Added normalizing string method

--- Desktop/workspace/ripoff/src/ripoff.cpp
+++ Desktop/workspace/ripoff/src/ripoff.cpp
@@ -15,16 +15,16 @@
 
 using namespace std;
 
-// Читаем файл
+/* Reading input file */
 vector <string> readFile() {
 	ifstream fileStream;
 	string currentLine;
 	vector <string> stringVector;
 
-	// Читаем файл
+	/* Opening file stream */
 	fileStream.open("input.txt");
 
-	// Записываем файл в вектор строк
+	/* Filling the Vector with file strings */
 	while(!fileStream.eof()) {
 		getline(fileStream, currentLine);
 		stringVector.push_back(currentLine);
@@ -35,14 +35,14 @@ vector <string> readFile() {
 	return stringVector;
 }
 
-// Выводим файл
+/* Printing Vector contents */
 void printStringVector(vector <string> stringVector) {
 	for(int i = 0; i < stringVector.size(); i++) {
 		cout << stringVector[i] << endl;
 	}
 }
 
-// Получаем стоп-слова в виде вектора
+/* Getting stop-words array */
 vector <string> getStopwords() {
 	string stopWordsArray[] = {
 			",", ".", ":", "«", "»", "—", "(", ")", "’", "!", "-", "=", "'", "[", "]"
@@ -57,28 +57,42 @@ vector <string> getStopwords() {
 	return stopWordsVector;
 }
 
-int main() {
-	setlocale(LC_ALL, "");
-	vector <string> stringVectorInput;
-	vector <string> stringVectorOutput;
-	vector <string> stopWords = getStopwords();
+/* String to lower case */
+vector <string> stringToLower(vector <string> Input) {
+	for(int i = 0; i < Input.size(); i++) {
+		transform(Input[i].begin(), Input[i].end(), Input[i].begin(), ::tolower);
+	}
 
-	stringVectorInput = readFile(); // Читаем файл
+	return Input;
+}
 
+/* Normalizing String */
+vector <string> normalizeString(vector <string> Input) {
 	int start;
 
+	vector <string> stopWords = getStopwords();
+
 	for(int i = 0; i < stopWords.size(); i++) {
-		for(int k = 0; k < stringVectorInput.size(); k++) {
-			while((start = stringVectorInput[k].find(stopWords[i])) != string::npos) {
-				stringVectorInput[k].replace(start, stopWords[i].length(), "");
+		for(int k = 0; k < Input.size(); k++) {
+			while((start = Input[k].find(stopWords[i])) != string::npos) {
+				Input[k].replace(start, stopWords[i].length(), "");
 			}
 		}
 	}
 
-	printStringVector(stringVectorInput);
+	Input = stringToLower(Input);
 
-	// @TODO Сделать возможность перевода в нижний регистр для русской кодировки
-	// Собрать все в одну строку
+	return Input;
+}
+
+int main() {
+	setlocale(LC_ALL, "");
+	vector <string> stringVectorInput;
+	vector <string> stringVectorOutput;
+
+	stringVectorInput = normalizeString(readFile()); // Reading and normalizing file
+
+	printStringVector(stringVectorInput);
 
 	return 0;
 }
